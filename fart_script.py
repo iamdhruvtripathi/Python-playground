4bacf6e90c660572d908c7459e6d16c255cfd3c0
SwiftCode123 <dhruv.tr03@gmail.com>
2026-09-07 00:42:05 -0500

    Added fart machine

---
+++ fart_script.py
@@ -0,0 +1,19 @@
+from pynput import keyboard
+from playsound3 import playsound
+
+def fart():
+    playsound("fart-with-reverb.mp3")
+
+def on_press(key):
+    try:
+        if key.char == "f":
+            fart()
+    except AttributeError:
+        if key == keyboard.Key.esc:
+            return False
+
+print("💨 Fart machine armed")
+print("Press F to fart. Press ESC to quit.")
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
