eb9a16edc143fb400fbfefa314a34974d003771c
SwiftCode123 <dhruv.tr03@gmail.com>
2026-09-07 01:17:05 -0500

    Made folder structure better

--- Fart machine/fart_script.py
+++ Fart machine/fart_script.py
@@ -2,7 +2,7 @@ from pynput import keyboard
 from playsound3 import playsound
 
 def fart():
-    playsound("MP3 files/fart-with-reverb.mp3")
+    playsound("fart-with-reverb.mp3")
 
 def on_press(key):
     try:
